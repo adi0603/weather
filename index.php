123ba2b49e5c7f23fe2662e318ed7695e47cabc6
Aditya Pandey <adityapandey0421@gmail.com>
2021-05-07 14:05:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,41 @@
 		 <meta name="description" content="">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <link rel="icon" type="image/ico" href="image/icon.ico">
-	        <link rel="stylesheet" type="text/css" href="style.css?rnd=132">	
+	       
+	<style>
+		body {
+			  	background: #efefef;
+			}
+			p {
+				left: 42%;
+			   	position: absolute;
+			   	top: 50%;
+			   	font-family: Helvetica, 'Helvetica Neue', sans-serif;
+				letter-spacing: 1px;
+				color: #a0a0a0;
+				text-align: center;
+			}
+			img {   
+			   	left: 45%;
+			   	position: absolute;
+			   	top: 40%;
+			}
+			@media only screen and (max-width: 360px) {
+			  	img {		   
+				  	left: 35%;
+				  	position: absolute;
+				  	top: 40%;
+				}
+				p{
+					left: 15%;
+				  	position: absolute;
+				  	top: 60%;
+				  	font-family: Helvetica, 'Helvetica Neue', sans-serif;
+				  	color: #a0a0a0;
+					text-align: center;
+				}
+			}
+		</style>
 	</head>
 	<body onload="myFunction()">
 		<div id="loader">
